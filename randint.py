19bc2a6e2b0f61529b73d1d7e38e59d77a2b6da8
laika <laika.catt@gmail.com>
2017-10-26 12:10:48 -0700

    added more functionality

--- randint.py
+++ randint.py
@@ -4,6 +4,7 @@
 #of previous guesses
 
 from random import randint
+import operator
 start = raw_input("have you previously selected values? y/n: ")
 flag = None
 if start == 'y':
@@ -11,14 +12,30 @@ if start == 'y':
 else:
     flag = False
 
-if not flag:
-    MAX = input("Largest ID previously selected: ")
+if  flag:
+    MAX = input("How Many ID's previously selected: ")
+    values = [0 for x in range(9)]
+    print (values)
+    print(operator.itemgetter(0)(values))
+    i = 0
+    while i < MAX:
+        values = input("Last ID selection from 0 to 9: ")
+        i = i + 1
 
-    print("Last ID selection from 0 to 9: ")
     #---------------------------------
-    #generates 2 psudo random integers
+    #generates psudo random integers
     #---------------------------------
-    rand = randint(MAX,9) #or n in range(0,1)
+    rand = randint(0,9) #or n in range(0,1)
+    j = 0
+    while j < MAX:
+        k = operator.itemgetter(0)(values)
+        print(j)
+        print(k)
+        if values == rand:
+            rand = randint(0,9)
+            j = j + 1
+        else:
+            j = j + 1
     print(rand)
 else:
     print(randint(0,9))
